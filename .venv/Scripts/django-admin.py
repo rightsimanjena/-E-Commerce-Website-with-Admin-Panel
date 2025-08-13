37ac2923adce67f69437d83f7ac2e81c628c024d
rightsimanjena <rightsimanjena14@gmail.com>
2025-08-13 19:04:13 +0530

    Initial Django project commit

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Right Siman Jena\My Real Life Projects\ECommerceSite-Django\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
